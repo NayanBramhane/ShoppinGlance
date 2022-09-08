1a81e002dc0377db3a33a0e9e63bf4680a22a314
NayanBramhane <nbramhane52@gmail.com>
2022-09-08 09:32:57 +0530

    Added all Products drop down list pages

---
+++ ShoppingSite/ManDenims.aspx.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Globalization;
+using System.Threading;
+using MySql.Data.MySqlClient;
+
+namespace ShoppingSite
+{
+    public partial class ManDenims : System.Web.UI.Page
+    {
+        public static String CS = ConfigurationManager.ConnectionStrings["MyShoppingDB"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Username"] != null)
+            {
+                if (!IsPostBack)
+                {
+                    if (Request.QueryString["BuyNow"] == "YES")
+                    {
+
+                    }
+                    BindProductRepeater();
+                    BindCartNumber();
+
+                }
+            }
+            else
+            {
+
+                if (Request.QueryString["BuyNow"] == "YES")
+                {
+                    Response.Redirect("SignIn.aspx");
+                }
+                else
+                {
+                    Response.Redirect("~/Default.aspx");
+                }
+            }
+        }
+        private void BindProductRepeater()
+        {
+            using (MySqlConnection con = new MySqlConnection(CS))
+            {
+                using (MySqlCommand cmd = new MySqlCommand("procBindAllProducts4", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (MySqlDataAdapter sda = new MySqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+                        rptrProducts.DataSource = dt;
+                        rptrProducts.DataBind();
+                        if (dt.Rows.Count <= 0)
+                        {
+                            // Label1.Text = "Sorry! Currently no products in this category.";
+                        }
+                        else
+                        {
+                            //Label1.Text = "Showing All Products";
+                        }
+                    }
+                }
+            }
+        }
+        protected override void InitializeCulture()
+        {
+            CultureInfo ci = new CultureInfo("en-IN");
+            ci.NumberFormat.CurrencySymbol = "₹";
+            Thread.CurrentThread.CurrentCulture = ci;
+
+            base.InitializeCulture();
+        }
+        public void BindCartNumber()
+        {
+            if (Session["USERID"] != null)
+            {
+                string UserIDD = Session["USERID"].ToString();
+                DataTable dt = new DataTable();
+                using (MySqlConnection con = new MySqlConnection(CS))
+                {
+                    MySqlCommand cmd = new MySqlCommand("SP_BindCartNumberz", con)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+                    cmd.Parameters.AddWithValue("_UserID", UserIDD);
+                    using (MySqlDataAdapter sda = new MySqlDataAdapter(cmd))
+                    {
+                        sda.Fill(dt);
+                        if (dt.Rows.Count > 0)
+                        {
+                            string CartQuantity = dt.Compute("Sum(Qty)", "").ToString();
+                            //CartBadge.InnerText = CartQuantity;
+                        }
+                        else
+                        {
+                            // _ = CartBadge.InnerText == 0.ToString();
+                            //CartBadge.InnerText = "0";
+                        }
+                    }
+                }
+            }
+        }
+
+        //private void SearchProductByTextbox()
+        //{
+        //    using (MySqlConnection con = new MySqlConnection(CS))
+        //    {
+        //        using (MySqlCommand cmd = new MySqlCommand("procBindAllProductsMansDenimFilter", con)) //make a stored procedure to find denims
+        //        {
+        //            cmd.CommandType = CommandType.StoredProcedure;
+        //            cmd.Parameters.AddWithValue("_TXT", txtFilterGrid1Record.Text);
+        //            using (MySqlDataAdapter sda = new MySqlDataAdapter(cmd))
+        //            {
+        //                DataTable dt = new DataTable();
+        //                sda.Fill(dt);
+        //                rptrProducts.DataSource = dt;
+        //                rptrProducts.DataBind();
+        //                if (dt.Rows.Count <= 0)
+        //                {
+        //                    // Label1.Text = "Sorry! Currently no products in this category.";
+        //                }
+        //                else
+        //                {
+        //                    //Label1.Text = "Showing All Products";
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+
+        protected void txtFilterGrid1Record_TextChanged(object sender, EventArgs e)
+        {
+            if (txtFilterGrid1Record.Text != string.Empty)
+            {
+                // SearchProductByTextbox();
+            }
+
+            MySqlConnection con = new MySqlConnection(CS);
+            con.Open();
+            string qr = "select A.*,B.*,C.Name ,A.PPrice-A.PSellPrice as DiscAmount,B.Name as ImageName, C.Name as BrandName from tblProducts A " +
+                "inner join tblBrands C on C.BrandID =A.PBrandID inner join tblCategory as t2 on t2.CatID=A.PCategoryID join lateral" +
+                "( select * from tblProductImages B where B.PID= A.PID order by B.PID desc limit 1)B where t2.CatName='Denim' or t2.CatName='Jeans' " +
+                "AND A.PName like '" + txtFilterGrid1Record.Text + "%' order by A.PID desc";
+            MySqlDataAdapter da = new MySqlDataAdapter(qr, con);
+            string text = ((TextBox)sender).Text;
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                rptrProducts.DataSource = ds.Tables[0];
+                rptrProducts.DataBind();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ ShoppingSite/ManShirt.aspx.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace ShoppingSite
+{
+    public partial class ManShirt : System.Web.UI.Page
+    {
+        public static String CS = ConfigurationManager.ConnectionStrings["MyShoppingDB"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Username"] != null)
+            {
+                if (!IsPostBack)
+                {
+                    if (Request.QueryString["BuyNow"] == "YES")
+                    {
+
+                    }
+                    BindProductRepeater();
+                    BindCartNumber();
+
+                }
+            }
+            else
+            {
+                if (Request.QueryString["BuyNow"] == "YES")
+                {
+                    Response.Redirect("SignIn.aspx");
+                }
+                else
+                {
+                    Response.Redirect("~/Default.aspx");
+                }
+            }
+        }
+        private void BindProductRepeater()
+        {
+            using (MySqlConnection con = new MySqlConnection(CS))
+            {
+                using (MySqlCommand cmd = new MySqlCommand("procBindAllProducts2", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (MySqlDataAdapter sda = new MySqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+                        rptrProducts.DataSource = dt;
+                        rptrProducts.DataBind();
+                        if (dt.Rows.Count <= 0)
+                        {
+                            // Label1.Text = "Sorry! Currently no products in this category.";
+                        }
+                        else
+                        {
+                            //Label1.Text = "Showing All Products";
+                        }
+                    }
+                }
+            }
+        }
+
+        protected override void InitializeCulture()
+        {
+            CultureInfo ci = new CultureInfo("en-IN");
+            ci.NumberFormat.CurrencySymbol = "₹";
+            Thread.CurrentThread.CurrentCulture = ci;
+
+            base.InitializeCulture();
+        }
+        public void BindCartNumber()
+        {
+            if (Session["USERID"] != null)
+            {
+                string UserIDD = Session["USERID"].ToString();
+                DataTable dt = new DataTable();
+                using (MySqlConnection con = new MySqlConnection(CS))
+                {
+                    MySqlCommand cmd = new MySqlCommand("SP_BindCartNumberz", con)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+                    cmd.Parameters.AddWithValue("_UserID", UserIDD);
+                    using (MySqlDataAdapter sda = new MySqlDataAdapter(cmd))
+                    {
+                        sda.Fill(dt);
+                        if (dt.Rows.Count > 0)
+                        {
+                            string CartQuantity = dt.Compute("Sum(Qty)", "").ToString();
+                            //CartBadge.InnerText = CartQuantity;
+                        }
+                        else
+                        {
+                            // _ = CartBadge.InnerText == 0.ToString();
+                            //CartBadge.InnerText = "0";
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void txtFilterGrid1Record_TextChanged(object sender, EventArgs e)
+        {
+            if (txtFilterGrid1Record.Text != string.Empty)
+            {
+                // SearchProductByTextbox();
+            }
+
+            MySqlConnection con = new MySqlConnection(CS);
+            con.Open();
+            string qr = "select A.*,B.*,C.Name ,A.PPrice-A.PSellPrice as DiscAmount,B.Name as ImageName, C.Name as BrandName from tblProducts A " +
+                "inner join tblBrands C on C.BrandID =A.PBrandID inner join tblCategory as t2 on t2.CatID=A.PCategoryID join lateral" +
+                "( select * from tblProductImages B where B.PID= A.PID order by B.PID desc limit 1)B where t2.CatName='Shirt' AND A.PName like '" +
+                txtFilterGrid1Record.Text + "%' order by A.PID desc";
+            MySqlDataAdapter da = new MySqlDataAdapter(qr, con);
+            string text = ((TextBox)sender).Text;
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                rptrProducts.DataSource = ds.Tables[0];
+                rptrProducts.DataBind();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
